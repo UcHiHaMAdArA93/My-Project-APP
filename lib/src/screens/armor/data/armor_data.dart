63ffec7268b094a85a750e234de38b87cc9f4c7b
Rafael <rafael.a.ferreirasantos93@gmail.com>
2024-08-22 15:04:57 +0200

    Added armor data and domain (class and got the lists ready)

---
+++ lib/src/screens/armor/data/armor_data.dart
@@ -0,0 +1,75 @@
+import 'package:conan_app/src/screens/armor/domain/armor_domain.dart';
+
+final List<ArmorModel> lightArmor = [
+  ArmorModel(
+  //1
+  /*Helmet*/ "", "", 
+  /*Chest*/ "", "", 
+  /*Gloves*/ "", "", 
+  /*Legs*/ "", "", 
+  /*Feet*/"", ""),
+  //2
+  ArmorModel(
+  /*Helmet*/ "", "", 
+  /*Chest*/ "", "", 
+  /*Gloves*/ "", "", 
+  /*Legs*/ "", "", 
+  /*Feet*/"", ""),
+  //3
+  ArmorModel(
+  /*Helmet*/ "", "", 
+  /*Chest*/ "", "", 
+  /*Gloves*/ "", "", 
+  /*Legs*/ "", "", 
+  /*Feet*/"", ""),
+];
+
+
+final List<ArmorModel> mediumArmor = [
+  ArmorModel(
+  //1
+  /*Helmet*/ "", "", 
+  /*Chest*/ "", "", 
+  /*Gloves*/ "", "", 
+  /*Legs*/ "", "", 
+  /*Feet*/"", ""),
+  //2
+  ArmorModel(
+  /*Helmet*/ "", "", 
+  /*Chest*/ "", "", 
+  /*Gloves*/ "", "", 
+  /*Legs*/ "", "", 
+  /*Feet*/"", ""),
+  //3
+  ArmorModel(
+  /*Helmet*/ "", "", 
+  /*Chest*/ "", "", 
+  /*Gloves*/ "", "", 
+  /*Legs*/ "", "", 
+  /*Feet*/"", ""),
+];
+
+
+final List<ArmorModel> heavyArmor = [
+  ArmorModel(
+  //1
+  /*Helmet*/ "", "", 
+  /*Chest*/ "", "", 
+  /*Gloves*/ "", "", 
+  /*Legs*/ "", "", 
+  /*Feet*/"", ""),
+  //2
+  ArmorModel(
+  /*Helmet*/ "", "", 
+  /*Chest*/ "", "", 
+  /*Gloves*/ "", "", 
+  /*Legs*/ "", "", 
+  /*Feet*/"", ""),
+  //3
+  ArmorModel(
+  /*Helmet*/ "", "", 
+  /*Chest*/ "", "", 
+  /*Gloves*/ "", "", 
+  /*Legs*/ "", "", 
+  /*Feet*/"", ""),
+];
---
+++ lib/src/screens/armor/domain/armor_domain.dart
@@ -0,0 +1,20 @@
+class ArmorModel {
+  String title1;
+  String imagePath1;
+  String title2;
+  String imagePath2;
+  String title3;
+  String imagePath3;
+  String title4;
+  String imagePath4;
+  String title5;
+  String imagePath5;
+
+  ArmorModel(
+  this.title1, this.imagePath1, 
+  this.title2, this.imagePath2, 
+  this.title3, this.imagePath3, 
+  this.title4, this.imagePath4, 
+  this.title5, this.imagePath5
+  );
+}
