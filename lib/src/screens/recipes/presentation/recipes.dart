e6ededc9cbf2024185149482252ea3c42bf6509d
Rafael <rafael.a.ferreirasantos93@gmail.com>
2024-08-22 15:47:25 +0200

    added armor tier screens

--- lib/src/screens/recipes/presentation/recipes.dart
+++ lib/src/screens/recipes/presentation/recipes.dart
@@ -1,4 +1,4 @@
-import 'package:conan_app/src/screens/armor/presentation/armor.dart';
+import 'package:conan_app/src/screens/armor/presentation/armor_category_screen.dart';
 import 'package:conan_app/src/screens/benches/presentation/benches.dart';
 import 'package:conan_app/src/screens/weapons/presentation/categories.dart';
 import 'package:flutter/material.dart';
