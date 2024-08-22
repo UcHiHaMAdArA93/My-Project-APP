c8b8bc9d97d12f2f22d121146e3c42d08180418a
Rafael <rafael.a.ferreirasantos93@gmail.com>
2024-08-22 16:07:38 +0200

    finished armor database

--- lib/src/screens/armor/data/armor_data.dart
+++ lib/src/screens/armor/data/armor_data.dart
@@ -3,73 +3,79 @@ import 'package:conan_app/src/screens/armor/domain/armor_domain.dart';
 final List<ArmorModel> lightArmor = [
   ArmorModel(
   //1
-  /*Helmet*/ "", "", 
-  /*Chest*/ "", "", 
-  /*Gloves*/ "", "", 
-  /*Legs*/ "", "", 
-  /*Feet*/"", ""),
+  /*Helmet*/ "Silent Legion Light Helmet", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/d/dd/Icon_silentlegion_lgt_m_helm.png/revision/latest/scale-to-width-down/32?cb=20210325185731", 
+  /*Chest*/ "Silent Legion Light Pauldron", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/5/58/Icon_silentlegion_lgt_m_top.png/revision/latest/scale-to-width-down/32?cb=20210325185754", 
+  /*Gloves*/ "Silent Legion Light Gauntlets", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/3/34/Icon_silentlegion_lgt_m_glove.png/revision/latest/scale-to-width-down/32?cb=20210325185713", 
+  /*Legs*/ "Silent Legion Light Tasset", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/3/3e/Icon_silentlegion_lgt_m_bottom.png/revision/latest/scale-to-width-down/32?cb=20210325185829", 
+  /*Feet*/"Silent Legion Light Boots", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/a/a9/Icon_silentlegion_lgt_m_boot.png/revision/latest/scale-to-width-down/32?cb=20210325185655"),
   //2
   ArmorModel(
-  /*Helmet*/ "", "", 
-  /*Chest*/ "", "", 
-  /*Gloves*/ "", "", 
-  /*Legs*/ "", "", 
-  /*Feet*/"", ""),
+  /*Helmet*/ "Abyssal Helm", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/6/6c/Icon_armor_helmet_demon.png/revision/latest/scale-to-width-down/32?cb=20220910211347", 
+  /*Chest*/ "Abyssal Mail", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/f/f0/Icon_armor_chest_demon.png/revision/latest/scale-to-width-down/32?cb=20220910211415", 
+  /*Gloves*/ "Abyssal Gloves", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/c/ca/Icon_armor_gloves_demon.png/revision/latest/scale-to-width-down/32?cb=20220910211433", 
+  /*Legs*/ "Abyssal Leggings", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/c/c9/Icon_armor_legs_demon.png/revision/latest/scale-to-width-down/32?cb=20220910211449", 
+  /*Feet*/"Abyssal Boots", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/2/25/Icon_armor_boots_demon.png/revision/latest/scale-to-width-down/32?cb=20220910211540"),
   //3
   ArmorModel(
-  /*Helmet*/ "", "", 
-  /*Chest*/ "", "", 
-  /*Gloves*/ "", "", 
-  /*Legs*/ "", "", 
-  /*Feet*/"", ""),
+  /*Helmet*/ "Perfected Light Turban", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/6/6d/Icon_light_exile_cap.png/revision/latest/scale-to-width-down/32?cb=20180507213158", 
+  /*Chest*/ "Perfected Light Chestpiece", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/3/3f/Icon_light_exile_armor.png/revision/latest/scale-to-width-down/32?cb=20180507213154", 
+  /*Gloves*/ "Perfected Light Gauntlets", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/c/c1/Icon_light_exile_gauntlets.png/revision/latest/scale-to-width-down/32?cb=20180507213159", 
+  /*Legs*/ "Perfected Light Wrap", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/3/3b/Icon_light_exile_loincloth.png/revision/latest/scale-to-width-down/32?cb=20180507213201", 
+  /*Feet*/"Perfected Light Boots", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/4/47/Icon_light_exile_boots.png/revision/latest/scale-to-width-down/32?cb=20180507213156"),
 ];
 
 
 final List<ArmorModel> mediumArmor = [
   ArmorModel(
   //1
-  /*Helmet*/ "", "", 
-  /*Chest*/ "", "", 
-  /*Gloves*/ "", "", 
-  /*Legs*/ "", "", 
-  /*Feet*/"", ""),
+  /*Helmet*/ "Silent Legion Medium Helmet", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/8/8b/Icon_silentlegion_med_m_helm.png/revision/latest/scale-to-width-down/32?cb=20210325185958", 
+  /*Chest*/ "Silent Legion Medium Pauldron", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/f/f4/Icon_silentlegion_med_m_top.png/revision/latest/scale-to-width-down/32?cb=20210325190015", 
+  /*Gloves*/ "Silent Legion Medium Gauntlets", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/a/a5/Icon_silentlegion_med_m_glove.png/revision/latest/scale-to-width-down/32?cb=20210325185943", 
+  /*Legs*/ "Silent Legion Medium Tasset", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/3/30/Icon_silentlegion_med_m_bottom.png/revision/latest/scale-to-width-down/32?cb=20210325190033", 
+  /*Feet*/"Silent Legion Medium Boots", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/6/61/Icon_silentlegion_med_m_boot.png/revision/latest/scale-to-width-down/32?cb=20210325185850"
+  ),
   //2
   ArmorModel(
-  /*Helmet*/ "", "", 
-  /*Chest*/ "", "", 
-  /*Gloves*/ "", "", 
-  /*Legs*/ "", "", 
-  /*Feet*/"", ""),
+  /*Helmet*/ "Helm of Sobek", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/d/d2/Icon_legendary_crocodile_male_helmet_white.png/revision/latest/scale-to-width-down/32?cb=20190626204926", 
+  /*Chest*/ "Chestguard of Sobek", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/c/ce/Icon_legendary_crocodile_male_top_white.png/revision/latest/scale-to-width-down/32?cb=20190626204747", 
+  /*Gloves*/ "Gauntlets of Sobek", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/3/34/Icon_legendary_crocodile_male_gloves_white.png/revision/latest/scale-to-width-down/32?cb=20190626204846", 
+  /*Legs*/ "Tasset of Sobek", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/0/0f/Icon_legendary_crocodile_male_bottom_white.png/revision/latest/scale-to-width-down/32?cb=20190626205008", 
+  /*Feet*/"Boots of Sobek", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/e/e6/Icon_legendary_crocodile_male_boots_white.png/revision/latest/scale-to-width-down/32?cb=20190626204702"
+  ),
   //3
   ArmorModel(
-  /*Helmet*/ "", "", 
-  /*Chest*/ "", "", 
-  /*Gloves*/ "", "", 
-  /*Legs*/ "", "", 
-  /*Feet*/"", ""),
+  /*Helmet*/ "Perfected Medium Cap", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/a/a9/Icon_Medium_exile_cap-1.png/revision/latest/scale-to-width-down/32?cb=20180507213239", 
+  /*Chest*/ "Perfected Medium Harness", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/f/f0/Icon_Medium_exile_harness-1.png/revision/latest/scale-to-width-down/32?cb=20180507213243", 
+  /*Gloves*/ "Perfected Medium Gauntlets", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/6/69/Icon_Medium_exile_gauntlets-1.png/revision/latest/scale-to-width-down/32?cb=20180507213241", 
+  /*Legs*/ "Perfected Medium Tasset", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/7/70/Icon_Medium_exile_tasset-1.png/revision/latest/scale-to-width-down/32?cb=20180507213245", 
+  /*Feet*/"Perfected Medium Boots", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/c/cf/Icon_Medium_exile_boots-1.png/revision/latest/scale-to-width-down/32?cb=20180507213238"
+  ),
 ];
 
 
 final List<ArmorModel> heavyArmor = [
   ArmorModel(
   //1
-  /*Helmet*/ "", "", 
-  /*Chest*/ "", "", 
-  /*Gloves*/ "", "", 
-  /*Legs*/ "", "", 
-  /*Feet*/"", ""),
+  /*Helmet*/ "Redeemed Legion Helmet", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/2/2c/Epic_icon_deathknight_helmet.png/revision/latest/scale-to-width-down/32?cb=20180509212813", 
+  /*Chest*/ "Redeemed Legion Pauldron", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/c/cb/Epic_icon_deathknight_top.png/revision/latest/scale-to-width-down/32?cb=20180509212823", 
+  /*Gloves*/ "Redeemed Legion Gauntlets", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/e/e6/Epic_icon_deathknight_gloves.png/revision/latest/scale-to-width-down/32?cb=20180509212803", 
+  /*Legs*/ "Redeemed Legion Tasset", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/6/6d/Epic_icon_deathknight_bottom.png/revision/latest/scale-to-width-down/32?cb=20180509212832", 
+  /*Feet*/"Redeemed Legion Boots", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/3/39/Epic_icon_deathknight_boots.png/revision/latest/scale-to-width-down/32?cb=20180509212754"
+  ),
   //2
   ArmorModel(
-  /*Helmet*/ "", "", 
-  /*Chest*/ "", "", 
-  /*Gloves*/ "", "", 
-  /*Legs*/ "", "", 
-  /*Feet*/"", ""),
+  /*Helmet*/ "Champion's Helmet", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/a/aa/Icon_legendary_ArenaMasterHelmet.png/revision/latest/scale-to-width-down/32?cb=20190626194431", 
+  /*Chest*/ "Champion's Chestguard", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/2/2f/Icon_legendary_ArenaMasterChest.png/revision/latest/scale-to-width-down/32?cb=20190626193909", 
+  /*Gloves*/ "Champion's Gauntlets", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/8/8d/Icon_legendary_ArenaMasterBracer.png/revision/latest/scale-to-width-down/32?cb=20190626194308", 
+  /*Legs*/ "Champion's Leggings", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/e/e2/Icon_legendary_ArenaMasterLeggings.png/revision/latest/scale-to-width-down/32?cb=20190626194536", 
+  /*Feet*/"Champion's Boots", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/2/2a/Icon_legendary_ArenaMasterSabatons.png/revision/latest/scale-to-width-down/32?cb=20190626193821"
+  ),
   //3
   ArmorModel(
-  /*Helmet*/ "", "", 
-  /*Chest*/ "", "", 
-  /*Gloves*/ "", "", 
-  /*Legs*/ "", "", 
-  /*Feet*/"", ""),
+  /*Helmet*/ "Perfected Heavy Helmet", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/c/c2/Icon_heavy_exile_helmet.png/revision/latest/scale-to-width-down/32?cb=20180507212149", 
+  /*Chest*/ "Perfected Heavy Pauldron", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/8/8b/Icon_heavy_exile_pauldron.png/revision/latest/scale-to-width-down/32?cb=20180507212152", 
+  /*Gloves*/ "Perfected Heavy Gauntlets", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/a/a3/Icon_heavy_exile_gauntlets.png/revision/latest/scale-to-width-down/32?cb=20180507212148", 
+  /*Legs*/ "Perfected Heavy Tasset", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/8/87/Icon_heavy_exile_tasset.png/revision/latest/scale-to-width-down/32?cb=20180507212155", 
+  /*Feet*/"Perfected Heavy Sabatons", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/0/07/Icon_heavy_exile_sabatons.png/revision/latest/scale-to-width-down/32?cb=20180507212153"
+  ),
 ];
