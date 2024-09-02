f9395ccdc38788a806b299e27e6cc3a56eae6cb5
Rafael <rafael.a.ferreirasantos93@gmail.com>
2024-09-02 16:28:16 +0200

    Edited benches List

--- lib/src/screens/benches/data/benches_data.dart
+++ lib/src/screens/benches/data/benches_data.dart
@@ -1,15 +1,15 @@
 import 'package:conan_app/src/screens/benches/domain/benches_domain.dart';
 
 final List<BenchModel> benches = [
-  BenchModel("", ""),
-  BenchModel("", ""),
-  BenchModel("", ""),
-  BenchModel("", ""),
-  BenchModel("", ""),
-  BenchModel("", ""),
-  BenchModel("", ""),
-  BenchModel("", ""),
-  BenchModel("", ""),
-  BenchModel("", ""),
-  BenchModel("", ""),
+  BenchModel("Campfire", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/8/89/Icon_campfire_1.png/revision/latest?cb=20180507204709"),
+  BenchModel("Improved Stove", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/2/23/Icon_improved_stove.png/revision/latest?cb=20180507212242"),
+  BenchModel("Furnace", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/9/96/Icon_furnace.png/revision/latest?cb=20180507211533"),
+  BenchModel("Improved Furnace", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/1/1a/Icon_improved_furnace.png/revision/latest?cb=20180507212240"),
+  BenchModel("Armorer's Bench", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/b/b2/Icon_armormaking_bench.png/revision/latest?cb=20180507201715"),
+  BenchModel("Improved Armorer's Bench", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/7/7d/Icon_lmproved_armorers_bench.png/revision/latest/scale-to-width-down/80?cb=20180423043007"),
+  BenchModel("Blacksmith's Bench", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/a/a4/Icon_metalworking_bench.png/revision/latest/scale-to-width-down/80?cb=20180507213253"),
+  BenchModel("Improved Blacksmith's Bench", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/e/e7/Icon_improved_blacksmiths_bench.png/revision/latest/scale-to-width-down/80?cb=20180507212238"),
+  BenchModel("Carpenter's Bench", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/9/96/Icon_woodworking_bench.png/revision/latest/scale-to-width-down/80?cb=20180508022419"),
+  BenchModel("Precision Firebowl Cauldron", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/c/c4/Icon_crafting_cauldron_t3_varB.png/revision/latest/scale-to-width-down/80?cb=20201102173016"),
+  BenchModel("Precision Alchemist's Bench", "https://static.wikia.nocookie.net/conanexiles_gamepedia/images/f/ff/Icon_crafting_alchemy_bench_t3_varB.png/revision/latest/scale-to-width-down/80?cb=20201102173054"),
 ];
