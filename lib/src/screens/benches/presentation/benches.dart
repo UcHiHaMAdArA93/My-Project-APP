116f2b47616b6908a445ae92f971052880a5d27e
Rafael <rafael.a.ferreirasantos93@gmail.com>
2024-09-03 09:25:34 +0200

    Benches screen added benches and images

--- lib/src/screens/benches/presentation/benches.dart
+++ lib/src/screens/benches/presentation/benches.dart
@@ -1,3 +1,4 @@
+import 'package:conan_app/src/screens/benches/data/benches_data.dart';
 import 'package:flutter/material.dart';
 
 class Benches extends StatelessWidget {
@@ -5,11 +6,12 @@ class Benches extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return SafeArea(
+      child: Scaffold(
         appBar: AppBar(
           toolbarHeight: 35,
           backgroundColor: Colors.lightBlue,
-          title: const Text("Benches Recipes"),
+          title: const Text("Weapons Recipes"),
         ),
         body: Stack(
           children: [
@@ -24,8 +26,57 @@ class Benches extends StatelessWidget {
                 ),
               ),
             ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10.0,
+                  mainAxisSpacing: 10.0,
+                  childAspectRatio: 1, 
+                ),
+                itemCount: benches.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: benches[index].imagePath.isNotEmpty
+                                ? SizedBox(
+                                  height: 70,
+                                  width: 70,
+                                  child: Image.network(
+                                      benches[index].imagePath,
+                                      fit: BoxFit.contain,
+                                      filterQuality: FilterQuality.high,
+                                    ),
+                                )
+                                : Container(),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            benches[index].title,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16.0,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
           ],
         ),
-      );
+      ),
+    );
   }
 }
