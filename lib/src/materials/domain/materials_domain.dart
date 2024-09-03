ac4b9f322bf7c5d5831d6b20a1fe48abc050f9e2
Rafael <rafael.a.ferreirasantos93@gmail.com>
2024-09-03 09:44:28 +0200

    worked on domain from materials

---
+++ lib/src/materials/domain/materials_domain.dart
@@ -0,0 +1,47 @@
+class Stone {
+  String title;
+  String imagePath;
+
+  Stone(this.title, this.imagePath);
+}
+
+class Wood {
+  String title;
+  String imagePath;
+
+  Wood(this.title, this.imagePath);
+}
+
+class Branch {
+  String title;
+  String imagePath;
+
+ Branch (this.title, this.imagePath);
+}
+
+class Twine {
+  String title;
+  String imagePath;
+
+  Twine (this.title, this.imagePath);
+}
+class Steel {
+  String title;
+  String imagePath;
+
+  Steel (this.title, this.imagePath);
+}
+
+class HDSteel {
+  String title;
+  String imagePath;
+
+  HDSteel (this.title, this.imagePath);
+}
+
+class HDBricks {
+  String title;
+  String imagePath;
+
+  HDBricks (this.title, this.imagePath);
+}
